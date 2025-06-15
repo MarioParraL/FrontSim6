9fc3bbfe21e386ff8b639dca3545ea942e6f9357
MarioParraL <mariovidovick@gmail.com>
2025-06-15 18:36:07 +0200

    simulacro

--- fresh-sim6/components/Header.tsx
+++ fresh-sim6/components/Header.tsx
@@ -3,7 +3,7 @@ import { FunctionalComponent } from "preact";
 const Header: FunctionalComponent = () => {
   return (
     <div class="Header">
-      <h2>Esto es un simulacro</h2>
+      <h2>Esto es un simulacro..</h2>
       <a href={"/characters"}>Buscar personajes</a>
       <a href={"/house"}>Filtar por casa</a>
       <a href={"/logout"}>Log-out</a>
